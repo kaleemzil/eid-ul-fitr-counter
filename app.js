ce5dc06e4ee8d91cd59a26bc279eaee288a18891
M. KALEEMULLAH KHAN QURESHI <136243025+kaleemzil@users.noreply.github.com>
2024-01-01 10:59:03 -0800

    eid ul fitr counter
    checking by the another way to calculate the another differerence

---
+++ app.js
@@ -0,0 +1,32 @@
+const days=document.getElementById('Days');
+const hours=document.getElementById('Hours');
+const minutes=document.getElementById('Minutes');
+const seconds=document.getElementById('Seconds');
+
+
+var ramdan = new Date('Apr 11, 2024 18:39:00').getTime();
+var x= setInterval(function() {
+    
+
+var now = new Date().getTime();
+var diff=ramdan-now;
+
+var d=Math.floor(diff/(1000*60*60*24));
+console.log(d)
+
+var h=Math.floor((diff%(1000*60*60*24))/(1000*60*60));
+console.log(h)
+
+var min= Math.floor((diff %(1000*60*60)) /(1000*60));
+console.log(min)
+
+var sec= Math.floor((diff%(1000*60) /(1000) ));
+console.log(sec)
+
+days.innerHTML=d;
+hours.innerHTML=h<10? '0'+h:h;
+minutes.innerHTML=min<10? '0'+min:min;
+seconds.innerHTML=sec<10? '0'+sec:sec;
+
+
+},1000);
